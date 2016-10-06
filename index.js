abcb120f99a54ee8a72855a4e9080b203d72c20f
Damodar Bashyal <dbashyal@gmail.com>
2016-10-06 13:46:15 +1100

    added sqs receive and delete

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var aws = require('aws-sdk');
-// process.env.ENV_API
 
 aws.config.update({
     "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
@@ -26,9 +25,9 @@ lowesSqs.getQueueUrl = function () {
 
 lowesSqs.sqs = new aws.SQS();
 
-lowesSqs._next = function (err, result, next) {
+lowesSqs._next = function (err, result, next, msg) {
     if(next != undefined && typeof next == 'function'){
-        next(err, result);
+        next(err, result, msg);
     } else {
         if(err != null)
             console.log(err);
@@ -40,7 +39,7 @@ lowesSqs._next = function (err, result, next) {
 // create new queue
 lowesSqs.create = function (next) {
     var params = {QueueName:process.env.AWS_QUEUE_NAME};
-    lowesSqs.sqs.createQueue(params, function(err, data) {lowesSqs._next(err, data, next)});
+    lowesSqs.sqs.createQueue(params, function(err, result) {lowesSqs._next(err, result, next)});
 };
 
 // send new message to queue
@@ -50,7 +49,26 @@ lowesSqs.send = function (msg, next) {
         QueueUrl: lowesSqs.getQueueUrl(),
         DelaySeconds: 0
     };
-    lowesSqs.sqs.sendMessage(params, function(err, data) {lowesSqs._next(err, data, next)});
+    lowesSqs.sqs.sendMessage(params, function(err, result) {lowesSqs._next(err, result, next, msg)});
+};
+
+// receive messages
+lowesSqs.receive = function (next) {
+    var params = {
+        QueueUrl: lowesSqs.getQueueUrl(),
+        MaxNumberOfMessages: 10,
+        VisibilityTimeout: 600 // Wait time for anyone else to process. (seconds)
+    };
+    lowesSqs.sqs.receiveMessage(params, function(err, result) {lowesSqs._next(err, result, next)});
+};
+
+// delete processed message
+lowesSqs.delete = function (receipt, next) {
+    var params = {
+        QueueUrl: lowesSqs.getQueueUrl(),
+        ReceiptHandle: receipt
+    };
+    lowesSqs.sqs.deleteMessage(params, function(err, result) {lowesSqs._next(err, result, next)});
 };
 
 module.exports = lowesSqs;
